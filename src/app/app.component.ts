7a563abab3140f7ebb7a0849e70b925bf6f39e78
AitBenOm <omar.benaissa@outlook.com>
2017-10-24 17:20:22 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+  forbidenUsernames= ['omar', 'chaimae'];
+
+  ngOnInit(){
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, [Validators.required, this.forbidenNames.bind(this)]),
+        'email' : new FormControl(null, [Validators.required, Validators.email])
+      }),
+
+     'gender' : new FormControl('male'),
+      'hobbies': new FormArray([])
+    });
+  }
+
+  onSubmit(){
+    console.log(this.signupForm);
+  }
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+  forbidenNames(control: FormControl): {[s: string]: boolean}{
+    if(this.forbidenUsernames.indexOf(control.value) !=-1){
+      return{
+        'nameIsForbiden': true};
+    }return null;
+  }
+}
